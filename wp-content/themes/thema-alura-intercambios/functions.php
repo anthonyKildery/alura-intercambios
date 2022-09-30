3ff172c61cbe486c46f4b248acaa876bccfff48f
anthonyKld <anthonytecinf@gmail.com>
2022-09-29 22:50:00 -0300

    Conteúdo das paginas
    1 - Adicionamos a fução add_theme_support( 'post-thumbnails' );
    2 - Fizemos um loop de post no arquivo index.php;

--- wp-content/themes/thema-alura-intercambios/functions.php
+++ wp-content/themes/thema-alura-intercambios/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+    add_theme_support:  adiciona fuções para o admin por imagems no site
+*/
 function alura_intercambios_registrando_menu() {
     register_nav_menu(
         'menu-principal',
@@ -8,7 +11,8 @@ function alura_intercambios_registrando_menu() {
 }
 add_action( 'init', 'alura_intercambios_registrando_menu' );
 
-function alura_intercambios_adicionando_suporte_ao_tema(){
+function alura_intercambios_adicionando_suporte_ao_tema() {
     add_theme_support( 'custom-logo' );
+    add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'alura_intercambios_adicionando_suporte_ao_tema' );
--- wp-content/themes/thema-alura-intercambios/index.php
+++ wp-content/themes/thema-alura-intercambios/index.php
@@ -1,8 +1,16 @@
 <?php 
     require_once "header.php"; 
-?>
 
-<h1>Olá Mundo</h1>
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            the_post_thumbnail();
+            the_title();
+            the_content();
+        endwhile;
+    else :
+        _e( 'Não foi encontrado nenhum contéudo dessa pagina' );
+    endif;
+?>
 
 <?php 
     require_once "footer.php";
