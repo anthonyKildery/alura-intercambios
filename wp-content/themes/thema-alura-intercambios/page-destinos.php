c138e1f97e443c984dedad093bf51169e4371309
anthonyKld <anthonytecinf@gmail.com>
2022-10-01 18:28:20 -0300

    Hierarquia dos templates
    Tiramos o conteúdo do arquivo "index.php";
    Adicionamos os arquivos "page-sobre-nos.php" e "destinos.php";
    Isso deixar o site mais dinâmico.

---
+++ wp-content/themes/thema-alura-intercambios/page-destinos.php
@@ -0,0 +1,9 @@
+<?php
+$estiloPagina = 'destinos.css';
+require_once 'header.php';
+?>
+
+<h1>Esse é o conteúda da pagina destino</h1>
+
+<?php
+require_once 'footer.php';
---
+++ wp-content/themes/thema-alura-intercambios/page-sobre-nos.php
@@ -0,0 +1,20 @@
+<?php
+$estiloPagina = 'sobre_nos.css';
+require_once 'header.php';
+
+if ( have_posts() ):
+    _e( '<main class="main-sobre-nos">' );
+    while ( have_posts() ): the_post();
+    the_post_thumbnail(
+        'post-thumbnail', array( 'class' => 'imagem-sobre-nos' ) 
+    );
+    _e( ' <div class="conteudo container-alura"> ' );
+        the_title( '<h2>', '</h2>' );
+        the_content();
+    _e( ' </div> ' );
+    endwhile;
+
+    _e( '</main>' );
+endif;
+
+require_once 'footer.php';
