92cee9eb4fac99b72ef46cb0e3c73a5bfc06705c
anthonyKld <anthonytecinf@gmail.com>
2022-10-12 16:28:26 -0300

    .
    .

--- wp-content/themes/thema-alura-intercambios/functions.php
+++ wp-content/themes/thema-alura-intercambios/functions.php
@@ -26,6 +26,20 @@ function alura_intercambios_registrando_post_costumizado() {
 }
 add_action( 'init', 'alura_intercambios_registrando_post_costumizado' );
 
+function alura_intercambios_registrando_post_costumizado_banner() {
+    register_post_type(
+        'banners',
+        array(
+            'labels'            =>      array( 'name' => 'Banner' ),
+            'public'            =>      true,
+            'menu_position'     =>      3,
+            'menu_icon'         =>      'dashicons-edit',
+            'supports'          =>      array( 'title', 'thumbanail' ),
+        )
+    );
+}
+add_action( 'init', 'alura_intercambios_registrando_post_costumizado_banner' );
+
 /*
     add_theme_support:  adiciona fuções para o admin por imagems no site
 */
@@ -42,3 +56,24 @@ function alura_intercambios_adicionando_suporte_ao_tema() {
     add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'alura_intercambios_adicionando_suporte_ao_tema' );
+
+function alura_intercambios_registrando_metabox() {
+    add_meta_box(
+        'ai_registrando_metabox',
+        'Texto para o banner da home',
+        'ai_funcao_callback',
+        'banners',
+    );
+}
+add_action( 'add_meta_boxes', 'alura_intercambios_registrando_metabox' );
+
+function ai_funcao_callback( $post ) {
+    ?>
+    <label for="texto_home_1">1 - Texto</label>
+    <input type="text" name="texto_home_1" style="width: 100%">
+    <br>
+    <br>
+    <label for="texto_home_2">2 - Texto</label>
+    <input type="text" name="texto_home_2" style="width: 100%">
+    <?php
+}
